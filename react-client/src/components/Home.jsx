d63e956f77805d7dd6d1aa6fbaf6ae02af4fa2b2
Patrick Ryan <patrickry07@gmail.com>
2019-06-10 15:34:58 -0500

    added components for breaking up of frontend

---
+++ react-client/src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+
+
+  render() {
+
+    return (
+      <div>
+        Home component rendering!
+      </div>
+    );
+  }
+}
+
+
+export default Home;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './components/Home.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -19,7 +20,8 @@ class App extends React.Component {
 
     return (
       <div>
-        <h1>CATS HELLO WORLD!!</h1>
+        <Home/>
+
       </div>
     );
   }
