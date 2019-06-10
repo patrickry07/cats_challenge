a48c1f54b5000921870ca5bb515844dbbb0e571a
Patrick Ryan <patrickry07@gmail.com>
2019-06-10 15:18:36 -0500

    updated the sql file to add in mock data

--- schema.sql
+++ schema.sql
@@ -6,11 +6,36 @@ USE levelset;
 
 CREATE TABLE Cats (
   id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
+  thumbnail varchar(250) NOT NULL,
+  name varchar(250) NOT NULL,
+  birthdate varchar(250) NOT NULL,
+  owner_name varchar(250) NOT NULL,
+  views int NOT NULL,
   PRIMARY KEY (ID)
-);
+);  
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+INSERT into Cats
+  (thumbnail, name, birthdate, owner_name, views )
+VALUES
+  ('https://res.cloudinary.com/dlqxzhifw/image/upload/v1560197249/nvwnqarwjpd4xwqg0m8p.jpg', 'JoJo', '2016-01-01', "Alex Crowley", 0);
+
+INSERT into Cats
+  (thumbnail, name, birthdate, owner_name, views )
+VALUES
+  ('https://res.cloudinary.com/dlqxzhifw/image/upload/v1560197376/ca1wpzeqjkrtoges6idi.webp', 'TigerLilly', '2012-02-02', 'Joseph Ryan', 0);
+
+  INSERT into Cats
+  (thumbnail, name, birthdate, owner_name, views )
+VALUES
+  ('https://res.cloudinary.com/dlqxzhifw/image/upload/v1560197442/esrkern6sjhwrpq2yj3r.webp', 'Eunice', '2004-03-03', 'Patrick Coogan', 0);
+
+  INSERT into Cats
+  (thumbnail, name, birthdate, owner_name, views )
+VALUES
+  ('https://res.cloudinary.com/dlqxzhifw/image/upload/v1560197756/evnp8aryrkuln31amiha.jpg', 'Delphine', '2018-04-04', 'Emily Eisenhauer', 0);
+
+  INSERT into Cats
+  (thumbnail, name, birthdate, owner_name, views )
+VALUES
+  ('https://res.cloudinary.com/dlqxzhifw/image/upload/v1560197775/di8y3ir81bip1o5dpeia.jpg', 'Dodger', '2001-05-05', 'Thomas Baldwin', 0);
+
